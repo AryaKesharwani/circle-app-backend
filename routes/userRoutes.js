afb2e97cec8e3487be8769be562f22ee60067a46
Arya kesarwani <aryakesharwani1234@gmail.com>
2024-02-27 19:08:12 +0530

    fix

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 // models
-const User = require("./models/user");
+const User = require("../models/user");
 
 // middleware
 const onlyPayer = require("../middleware/onlyPayer");
