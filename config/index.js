e27e7e3bdb72db2bcaa104d1f3ddc9adf93a9d99
Arya kesarwani <aryakesharwani1234@gmail.com>
2024-02-27 21:45:03 +0530

    added config for db

--- config/index.js
+++ config/index.js
@@ -1,7 +1,9 @@
+require('dotenv').config();
+
 const mongoose = require('mongoose');
 
 
-mongoose.connect('mongodb://localhost:27017/', {
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
@@ -10,3 +12,6 @@ db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
   console.log("Connected to MongoDB");
 });
+
+
+module.exports = db;
