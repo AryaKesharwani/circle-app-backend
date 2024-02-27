b3bdc4212280bf6d7f6d065d2130fa05818e873d
Arya kesarwani <aryakesharwani1234@gmail.com>
2024-02-27 11:36:00 +0530

    added dotenv package

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
+require("dotenv").config();
+
 // middleware
 const verifyAuthToken = require("./middleware/verifyAuthToken");
 
